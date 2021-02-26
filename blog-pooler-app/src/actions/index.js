9ce1833684cb6c9897e9bd32d3fd651a71673a82
Scott Campbell <stc9486@gmail.com>
2021-02-25 19:27:48 -0500

    apply thunk

--- blog-pooler-app/src/actions/index.js
+++ blog-pooler-app/src/actions/index.js
@@ -1,5 +1,10 @@
-export const fetchPosts = () => {
+import jsonAPI from '../apis/jsonAPI';
+
+export const fetchPosts = async () => {
+	const response = await jsonAPI.get('/posts');
+
 	return {
 		type: 'FETCH_POSTS',
+		payload: response,
 	};
 };
--- blog-pooler-app/src/index.js
+++ blog-pooler-app/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './App';
 import reducers from './reducers';
 
+const store = createStore(reducers, applyMiddleware(thunk));
+
 ReactDOM.render(
 	<React.StrictMode>
-		<Provider store={createStore(reducers)}>
+		<Provider store={store}>
 			<App />
 		</Provider>
 	</React.StrictMode>,
