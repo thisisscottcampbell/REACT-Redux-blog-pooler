896728f22d9189a387b7febafded2ba0beaa9d9a
Scott Campbell <stc9486@gmail.com>
2021-03-01 19:54:39 -0500

    render list

--- blog-pooler-app/src/components/PostList.js
+++ blog-pooler-app/src/components/PostList.js
@@ -5,6 +5,19 @@ import { fetchPosts } from '../actions';
 const PostList = (props) => {
 	const { fetchPosts, posts } = props;
 
+	const blogList = () => {
+		return posts.map((post) => (
+			<div className="item" key={post.id}>
+				<i className="large middle aligned icon user" />
+				<div className="content">
+					<div className="description">
+						<h2>{post.title}</h2>
+						<p>{post.body}</p>
+					</div>
+				</div>
+			</div>
+		));
+	};
 	//cdm
 	useEffect(() => {
 		fetchPosts();
@@ -18,6 +31,7 @@ const PostList = (props) => {
 	return (
 		<div>
 			<h4>i am PostList</h4>
+			<div className="ui relaxed divided list">{() => blogList()}</div>
 		</div>
 	);
 };
